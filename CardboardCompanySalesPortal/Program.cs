260f96484344ca9f5f597654e9096f7369795dd1
Todd Spainhour <toddspainhour@gmail.com>
2020-08-27 20:28:07 -0500

    Main menu created. Program exits successfully

--- CardboardCompanySalesPortal/Program.cs
+++ CardboardCompanySalesPortal/Program.cs
@@ -7,6 +7,51 @@ namespace CardboardCompanySalesPortal
         static void Main(string[] args)
         {
             Console.WriteLine("Cardboard Company Sales Portal!");
+
+            Console.WriteLine("Welcome to Dufflin/Munder Cardboard Co. Sales Portal!");
+
+
+            var selectedMenuOption = "";
+
+
+            do
+            {
+                Console.WriteLine("Pick a number from the menu below.\n");
+
+                Console.WriteLine("1. Enter New Sale");
+                Console.WriteLine("2. Generate Report For Accountant");
+                Console.WriteLine("3. Add New Sales Employee");
+                Console.WriteLine("4. Find a Sale");
+                Console.WriteLine("5. Exit\n");
+
+
+                selectedMenuOption = Console.ReadLine();
+
+
+                switch (selectedMenuOption)
+                {
+                    case "1":
+                        Console.WriteLine("\nYou picked Option 1 \n");
+                        break;
+
+                    case "2":
+                        Console.WriteLine("\nYou picked Option 2 \n");
+                        break;
+
+                    case "3":
+                        Console.WriteLine("\nYou picked Option 3 \n");
+                        break;
+
+                    case "4":
+                        Console.WriteLine("\nYou picked Option 4 \n");
+                        break;
+
+                    default:
+                        Console.WriteLine("\n--- Please try again. --- \n");
+                        break;
+                }
+            }
+            while (selectedMenuOption != "5");
         }
     }
 }
